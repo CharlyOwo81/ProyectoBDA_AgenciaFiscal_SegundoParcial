8798600dc7c22d7fd5bfff46ab265c7954f66a77
CharlyOwo81 <150296234+CharlyOwo81@users.noreply.github.com>
2024-03-20 20:59:14 -0700

    Creación del proyecto "AgenciaFiscal_Negocios"

---
+++ AgenciaFiscal_Negocios/src/main/java/org/itson/bdavanzadas/agenciafiscal_negocios/AgenciaFiscal_Negocios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.bdavanzadas.agenciafiscal_negocios;
+
+/**
+ *
+ * @author gamaliel
+ */
+public class AgenciaFiscal_Negocios {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
